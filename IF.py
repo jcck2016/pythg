d6c3f212310ec2e3a07648e8435fde58d9350fa6
Jason Chu <jcck2000@yahoo.com>
2023-10-19 20:33:14 -0700

    added if.py from work laptop

---
+++ IF.py
@@ -0,0 +1,13 @@
+age = input ("how old are you?     ")
+age = int(age)
+
+if age <= 20:
+    print ("go home kid")
+
+elif age <= 80:
+    print ("come in")
+    
+else:
+    print ("you shouldnt be here")
+
+
