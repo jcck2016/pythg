f68ba6076770d70ea1b163c12e21da88c509050f
Jason Chu <jcck2000@yahoo.com>
2023-10-21 23:28:52 -0700

    added hi.py, praticing while loop !

---
+++ hi.py
@@ -0,0 +1,9 @@
+
+answer = input("please type the word 'hi': ")
+
+while answer != "hi":
+    print("plesae REALLY say 'hi' ")
+    answer = input("please type the EXACT word 'hi': ")
+
+
+print("HI THERE !!!!!!!!!")
