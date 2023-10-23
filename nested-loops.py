7a49e296f9b88148806e2d6d1c42cdb9ccae2d7d
Jason Chu <jcck2000@yahoo.com>
2023-10-23 16:56:51 -0700

    added nested loop lesson

---
+++ nested-loops.py
@@ -0,0 +1,9 @@
+
+
+for outer in range (1,5):
+    print("This is outer loop")
+    print(outer)
+    for inner in range (1,10):
+        print("\t", "This is inner loop")
+        print("\t",inner)
+    
